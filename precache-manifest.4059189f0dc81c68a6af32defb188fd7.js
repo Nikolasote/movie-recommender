7594629a861ec95b418a5f3e968aa15caf7a7fe1
Niklas Böcking <niklasboecking@yahoo.de>
2020-05-23 17:52:01 +0200

    Updates

---
+++ precache-manifest.4059189f0dc81c68a6af32defb188fd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c74901a3f2789f3c2312a79e75cd532",
+    "url": "/movie-recommender/index.html"
+  },
+  {
+    "revision": "4e0c0be3a6e8079eb4d2",
+    "url": "/movie-recommender/static/css/main.7dfa9707.chunk.css"
+  },
+  {
+    "revision": "07e5b5efd9b30dc45be0",
+    "url": "/movie-recommender/static/js/2.f01b538d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-recommender/static/js/2.f01b538d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0c0be3a6e8079eb4d2",
+    "url": "/movie-recommender/static/js/main.d5ba55ef.chunk.js"
+  },
+  {
+    "revision": "2c81ca55c50d9e1d13be",
+    "url": "/movie-recommender/static/js/runtime-main.730b8c8c.js"
+  },
+  {
+    "revision": "76fd6aefb3ed8613c25289ab35a8732d",
+    "url": "/movie-recommender/static/media/attribution-logos.76fd6aef.png"
+  }
+]);
