f7bee67fbe3c9518e002d0ba4ce73d0f70618139
Niklas Böcking <niklasboecking@yahoo.de>
2020-06-12 17:40:26 +0200

    viewcode link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,24 @@
-import React from "react"
-import '../css/main.css'
-import logos from "../images/attribution-logos.png"
+import React from "react";
+import "../css/main.css";
+import logos from "../images/attribution-logos.png";
 
 function Footer() {
-    return (
-        <div className="row footer-container justify-content-center mt-5">
-            <div className="col-12 text-center">
-                <p>designed by Niklas Böcking</p>
-                <a href="github.com">view code</a>
-            </div>
-            <div className="col-4 mt-3 text-center">
-                <p>powered by</p>
-                <img className="logos" src={logos} alt="powered by tastedive and TMDB" />
-            </div>
-        </div>
-    )
+  return (
+    <div className="row footer-container justify-content-center mt-5">
+      <div className="col-12 text-center">
+        <p>designed by Niklas Böcking</p>
+        <a href="https://github.com/Nikolasote/movie-recommender">view code</a>
+      </div>
+      <div className="col-4 mt-3 text-center">
+        <p>powered by</p>
+        <img
+          className="logos"
+          src={logos}
+          alt="powered by tastedive and TMDB"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
